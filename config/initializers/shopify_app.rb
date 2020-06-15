04e80d4dd7d0cc0d54064070df1b6136fd2ae6a9
Vanessa Santo <vanessaesanto@gmail.com>
2020-06-15 14:43:10 +0100

    update scope

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,9 +1,9 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "DriveFx Bridge"
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
   config.old_secret = ""
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "read_products, write_products, read_product_listings, read_orders, write_orders, read_inventory, write_inventory, read_locations, read_checkouts, write_checkouts, read_reports, write_reports" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
