53bf830b0ecd47a69b760fc6d8c89c0ccd86043b
Vanessa Santo <vanessaesanto@gmail.com>
2020-06-16 16:05:49 +0100

    complete DriveFx methods for fetching records - v1

--- app/services/fetch_drive_fx_service.rb
+++ app/services/fetch_drive_fx_service.rb
@@ -3,50 +3,32 @@ require 'uri'
 require 'json'
 
 class FetchDriveFxService
-  # Generate DriveFx API access token
+  # Generates DriveFx API Access Token
   def generate_access_token
-    # Raise error if any of the required credentials are missing
+    # Raises an error if any of the required credentials are missing
     env_vars = [ENV['DFX_BEND_URL'], ENV['DFX_APP_ID'], ENV['DFX_USERCODE'],
                 ENV['DFX_PASSWORD']]
-    raise 'Missing credentials' if env_vars.any? { |var| var.empty? }
+    raise 'Missing credentials' if env_vars.any?(&:empty?)
 
     # POST request details
     uri = URI.parse('https://interface.phcsoftware.com/v3/generateAccessToken')
     header = { 'Content-Type': 'application/json' }
     credentials =
       { 'credentials': {
-          'backendUrl': ENV['DFX_BEND_URL'],
-          'appId': ENV['DFX_APP_ID'],
-          'userCode': ENV['DFX_USERCODE'],
-          'password': ENV['DFX_PASSWORD'],
-          'company': '',
-          'tokenLifeTime': 'Never'
+        'backendUrl': ENV['DFX_BEND_URL'],
+        'appId': ENV['DFX_APP_ID'],
+        'userCode': ENV['DFX_USERCODE'],
+        'password': ENV['DFX_PASSWORD'],
+        'company': '',
+        'tokenLifeTime': 'Never'
       } }
 
-    # Create the HTTP objects
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Post.new(uri.request_uri, header)
-    request.body = credentials.to_json
-
-    # Send the request
-    response = http.request(request)
-    token_serialized = JSON.parse response.read_body
-
-    # Return token as string
-    token_serialized['token']
+    # Calls the API and returns the Access Token
+    api_call(uri, header, credentials)
   end
 
-  # Get all the products on 'armazem 1'
+  # Gets all the products on 'armazem 1'
   def fetch_warehouse_products(generate_access_token)
-    # Raise an error if the API token is missing
-    raise 'Missing API access token' if generate_access_token.empty?
-
-    # POST request details
-    uri = URI.parse('https://interface.phcsoftware.com/v3/searchEntities')
-    header =
-      { 'Content-Type': 'application/json',
-        'Authorization': generate_access_token }
     search_params =
       {
         'queryObject': {
@@ -59,23 +41,68 @@ class FetchDriveFxService
             'valueItem': 1,
             'groupItem': 0
           }],
-        'offset': 0,
-        'orderByItems': [],
-        'SelectItems': []
+          'offset': 0,
+          'orderByItems': [],
+          'SelectItems': []
         }
       }
 
-    # Create the HTTP objects
+    # Calls the fetch method to call the API and fetches products
+    fetch(generate_access_token, search_params)
+  end
+
+  # Gets the product details for a specific product reference
+  def fetch_product_details(generate_access_token, product_ref)
+    search_params =
+      {
+        'queryObject': {
+          'distinct': false,
+          'entityName': 'St',
+          'filterCod': '',
+          'filterItems': [{
+            'filterItem': 'ref',
+            'comparison': 0,
+            'valueItem': product_ref,
+            'groupItem': 0
+          }],
+          'limit': 20,
+          'offset': 0,
+          'orderByItems': [],
+          'SelectItems': []
+        }
+      }
+
+    # Calls the fetch method to call the API and fetches specific product
+    fetch(generate_access_token, search_params)
+  end
+
+  # Method that fetches records from the DriveFx API
+  def fetch(generate_access_token, search_params)
+    # Raises an error if the API token is missing
+    raise 'Missing API access token' if generate_access_token.empty?
+
+    # POST request details
+    uri = URI.parse('https://interface.phcsoftware.com/v3/searchEntities')
+    header =
+      { 'Content-Type': 'application/json',
+        'Authorization': generate_access_token }
+
+    api_call(uri, header, search_params)
+  end
+
+  # Method that calls the DriveFx API
+  def api_call(uri, header, search_params)
+    # Creates the HTTP objects
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     request = Net::HTTP::Post.new(uri.request_uri, header)
     request.body = search_params.to_json
 
-    # Send the request
+    # Sends the request
     response = http.request(request)
-    warehouse_serialized = JSON.parse response.read_body
+    response_serialized = JSON.parse response.read_body
 
-    # Return array of all products on 'armazem 1'
-    warehouse_serialized['entities']
+    # Returns the elements of the last key-value pair of response's body
+    response_serialized.values.last
   end
 end
